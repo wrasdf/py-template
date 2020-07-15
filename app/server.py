ac81d63d13fbe03e03d9c0f245daf0d66e6ffc62
Kerry Wang <wrasdf@gmail.com>
2020-07-15 17:09:45 +1000

    new py template

---
+++ app/server.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from app.services.response_manager import ResponseManager
+
+app = Flask("py-app")
+responseManager = ResponseManager()
+
+@app.after_request
+def add_header(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Content-Type"] = "application/json"
+    return response
+
+@app.route("/", methods=["GET"])
+def home():
+    return '''<h1>PY Template App</h1><p>Practice in local.</p>'''
+
+@app.route("/v1/healthz", methods=["GET"])
+def check_healthz():
+    return responseManager.success({"msg": "It works."})
+
+@app.route("/v1/failed", methods=["GET"])
+def check_failed():
+    return responseManager.error({"msg": "Server Failed"})
---
+++ app/services/response_manager.py
@@ -0,0 +1,19 @@
+from flask import jsonify, make_response
+
+# message = {
+#     "message": "It works.",
+# }
+
+class ResponseManager:
+    def __init__(self):
+        self.success_status_code = 200
+        self.error_status_code = 503
+
+    def response(self, message, status_code=200):
+        return make_response(jsonify(message)), status_code
+
+    def success(self, message):
+        return make_response(jsonify(message)), self.success_status_code
+
+    def error(self, message):
+        return make_response(jsonify(message)), self.error_status_code
