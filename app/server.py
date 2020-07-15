edb76290d4b83a618afaebe559bb18b57e3acc86
Kerry Wang <wrasdf@gmail.com>
2020-07-15 17:13:52 +1000

    clean up

--- app/server.py
+++ app/server.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from app.services.response_manager import ResponseManager
+from app.services.res_manager import ResponseManager
 
 app = Flask("py-app")
 responseManager = ResponseManager()
